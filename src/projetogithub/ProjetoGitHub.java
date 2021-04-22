35ebee4b500953bb2fc5fc379faad41d25039137
Jeferson Leon <jefersonleonblue@gmail.com>
2021-04-22 15:21:04 -0300

    Projeto Criado dia 22/04/2021 - Exemplo de como subir meu projeto no Github - foi criado no main o comando SOUT() - MINHA MENSAGEM

---
+++ src/projetogithub/ProjetoGitHub.java
@@ -0,0 +1,12 @@
+package projetogithub;
+
+/**
+ *
+ * @author Jeferson
+ */
+public class ProjetoGitHub {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!!! - O meu primeiro projeto no GitHub");
+    }
+    
+}
